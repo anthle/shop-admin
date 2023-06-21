44cd4e4e9026c674b34c0f91d23c7c51fe37657b
anthle <su810152974@gmail.com>
2023-06-21 23:54:45 +0800

    feat(setting-base): complete the page

---
+++ src/service/main/sysconfig.ts
@@ -0,0 +1,9 @@
+import request from '../request'
+
+export function getSysConfig() {
+	return request.get('/admin/sysconfig')
+}
+
+export function setSysConfig(data: any) {
+	return request.post('/admin/sysconfig', data)
+}
